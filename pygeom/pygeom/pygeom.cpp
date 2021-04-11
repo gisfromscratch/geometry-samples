063a0acdf8a9b0994570d0cb1d8dbaf64c25b02f
J.T <gisfromscratch@live.de>
2021-04-11 14:59:05 +0200

    Added Google unit tests

--- pygeom/pygeom/pygeom.cpp
+++ pygeom/pygeom/pygeom.cpp
@@ -3,10 +3,17 @@
 
 #include "pygeom.h"
 
+#include <gtest/gtest.h>
+
 using namespace std;
 
-int main()
+TEST(load_features, wkt_test)
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
